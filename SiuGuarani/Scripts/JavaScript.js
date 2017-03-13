0fd4a819ebf393b93406282aeef23a458812327f
Emanuel <emaaique@gmail.com>
2017-03-12 23:14:45 -0300

    SiuGuaraniPrimeraVersion :

---
+++ SiuGuarani/Scripts/JavaScript.js
@@ -0,0 +1,26 @@
+﻿$(Document).ready(function () {
+    // Get the modal
+
+    var modal = document.getElementById('modalAlumno');
+    var modal2 = document.getElementById('modalDocente');
+    var modal3 = document.getElementById('modalAdmin');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    window.onclick = function (event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
+    window.onclick = function (event) {
+        if (event.target == modal3) {
+            modal3.style.display = "none";
+        }
+    }
+
+ 
+});
